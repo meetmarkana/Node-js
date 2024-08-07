42f62a6a7879b72ff1f33c819aa7b4c2e931828c
meetmarkana <meetmarakna2078@gmail.com>
2024-08-07 17:52:40 +0530

    todo

--- TODO/todo.js
+++ TODO/todo.js
@@ -28,8 +28,8 @@ app.get("/", (req, res) => {
 app.post("/insertData", (req, res) => {
   const { id, name } = req.body;
   let obj = {
-    id: id,
-    name: name,
+    id,
+    name,
   };
   task.push(obj);
   res.redirect("/");
@@ -45,19 +45,35 @@ app.get("/deleteData", (req, res) => {
 app.get("/editdata", (req, resp) => {
   const id = req.query.editid;
   const data = task.find((el) => el.id == id);
+  // resp.send(data)
   resp.render("edit", {
-    data,
+    data
   });
 });
 
 app.post("/updatedata", (req, resp) => {
-  const { id, name } = req.body;
-  const todo_id = parseInt(id);
-  const todo = task.find((el) => el.id === todo_id);
 
-  if (todo) {
-    todo.name = name;
-  }
+
+  const { id,name } = req.body;
+  const userid = parseInt(id);
+  // const todo_id = parseInt(id);
+  // const todo = task.map((el) => {
+  //   if(el.id == id){
+  //     return {id,name}
+  //   }
+
+  //   return el
+  // });
+
+  const data = task.find(el=>el.id == userid)
+
+  data.id = userid
+  data.name = name
+
+  
+  // resp.send(todo)
+
+
   resp.redirect("/");
 });
 
@@ -74,5 +90,5 @@ app.get("/admin",middleware, (req, res) => {
 app.use(middleware);
 
 app.listen(7805, () => {
-  console.log("server listening on port 4545");
+  console.log("server listening on port 7805");
 });
