2b6cde403c7593001d6d337462ae3234408a508e
meetmarkana <meetmarakna2078@gmail.com>
2024-09-05 14:27:44 +0530

    MVC With Try Catch

--- MVC First/controllers/webctl.js
+++ MVC First/controllers/webctl.js
@@ -1,10 +1,24 @@
-module.exports.home=(req,res)=>{
-    res.render("home")
+module.exports.home=async(req,res)=>{
+   try{
+        await res.render("home")
+   }catch(err){
+    console.log("Somethig went wrong to reach home page" + err);
+    
+   }
 }
-module.exports.about=(req,res)=>{
-       res.render("about")
+module.exports.about=async(req,res)=>{
+    try{
+         await res.render("about")
+    }catch(err){
+     console.log("Somethig went wrong to reach about page" + err);
+     
+    }
 }
-module.exports.contact=(req,res)=>{
-    res.render("contact")
+module.exports.contact=async(req,res)=>{
+    try{
+         await res.render("contact")
+    }catch(err){
+     console.log("Somethig went wrong to reach contact page" + err);
+     
+    }
 }
-      
