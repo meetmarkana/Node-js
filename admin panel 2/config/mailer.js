8ae1448c60b449ba0cc30eb257d3827ab66fd08d
meetmarkana <meetmarakna2078@gmail.com>
2024-10-07 17:12:13 +0530

    Admin Pannel - 2 - project

---
+++ admin panel 2/config/mailer.js
@@ -0,0 +1,20 @@
+const nodemailer = require("nodemailer");
+
+const trasnsport = nodemailer.createTransport({
+    service : "gmail",
+    auth : {
+        user : "meetmarakna07@gmail.com",
+        pass : "yofrktlaultpldhj"
+    }
+})
+module.exports.sendotp = (to,otp)=>{
+    let mailoption ={
+        from : "meetmarakna07@gmail.com",
+        to : to,
+        subjet : "Your otp",
+        text : `your otp is ${otp}`
+    }
+    trasnsport.sendMail(mailoption,(err)=>{
+        err && console.log(err);
+    })
+}
